2acd321835bcf191de09f3bca76a88268d2f45b1
Jisse Reitsma <jisse@yireo.com>
2018-06-11 08:39:01 +0100

    Pending changes

--- registration.php
+++ registration.php
@@ -4,15 +4,14 @@
  *
  * @package     Yireo_ExampleThemeSkeleton
  * @author      Yireo (https://www.yireo.com/)
- * @copyright   Copyright 2017 Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2018 Yireo (https://www.yireo.com/)
  * @license     OSL
  */
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-$themeDir = basename(__DIR__);
 ComponentRegistrar::register(
     ComponentRegistrar::THEME,
-    'frontend/Yireo/'.$themeDir,
+    'frontend/Yireo/example',
     __DIR__
 );
